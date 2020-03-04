23957b0935abc8d9e77a6323e773246e6720ce6d
jpark2320 <jpark2320@gmail.com>
2020-03-04 15:18:16 -0500

    Initiate a Maven project with Spring Boot

---
+++ src/main/java/com/jaehong/stringbootstarter/topic/TopicController.java
@@ -0,0 +1,19 @@
+package com.jaehong.stringbootstarter.topic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("spring", "Spring", "Spring fun"),
+                new Topic("spring", "Spring", "Spring fun")
+        );
+    }
+}
